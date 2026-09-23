819bdfdcb7a7cb89af780a3bf88a31a644d35d53
awp-finanznachrichten <sw@awp.ch>
2026-09-23 09:50:59 +0200

    commit from Rstudio

--- top_cantons/run_top_flop_cant.r
+++ top_cantons/run_top_flop_cant.r
@@ -1,5 +1,5 @@
 source("./top_cantons/retrieve_data.R")
-source(".top_cantons/import_cant_res.R")
+source("./top_cantons/import_cant_res.R")
 source("./top_cantons/tabelle_builder.R")
 source("./top_cantons/plot_datawrapper.R")
 
